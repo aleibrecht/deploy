3b2b51abeb47519364e98da7f70cd03034942bb9
aleibrecht <aleibrecht@gmail.com>
2015-05-11 22:38:53 -0300

    Update autodeploy.sh

--- autodeploy.sh
+++ autodeploy.sh
@@ -1,1 +1,10 @@
-!#
+#!/bin/bash
+
+
+echo "Comienza la instalación"
+apt-get update
+apt-get install -y \
+           python-dev swig python-pip python-dev python-lxml libxml2-dev libxslt-dev \
+           postgresql postgresql-client postgresql-server-dev-all libpq-dev \
+           make git mercurial unoconv virtualenvwrapper
+pip install -r requirements.txt
